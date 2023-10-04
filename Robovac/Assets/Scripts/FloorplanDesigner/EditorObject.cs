139ccea51fa0209090ec95069d14ea79cdcc12ad
John Ingram <j123j7@gmail.com>
2023-10-04 12:29:25 -0500

    Start taking stuff from tutorial
    I have decided that we may be better off just rolling our own Level editor based on this short tutorial from reddit:
    https://www.reddit.com/r/Unity3D/comments/60398i/making_a_3d_level_editor_for_unity_game/

---
+++ Robovac/Assets/Scripts/FloorplanDesigner/EditorObject.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EditorObject : MonoBehaviour
+{
+    public enum ObjectType { Cylinder, Cube, Sphere, Player};
+    [Serializable]
+    public struct Data
+    {
+        public Vector3 pos;
+        public Quaternion rot;
+        public ObjectType objectType;
+    }
+    public Data data;
+}
